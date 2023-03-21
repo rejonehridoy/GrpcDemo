59eeebdfa62ab57e0b05aa7463000ed8fc747f6a
Rejone E Rasul Hridoy <Rejone.Rasul@brainstation-23.com>
2023-03-21 16:25:11 +0600

    production swagger ui added

--- ShoppingCartService/Startup.cs
+++ ShoppingCartService/Startup.cs
@@ -46,12 +46,9 @@ namespace ShoppingCartService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingCartService v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingCartService v1"));
 
             app.UseHttpsRedirection();
 
